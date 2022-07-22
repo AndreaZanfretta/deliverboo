1e9ef0831bd77649442244a90e022d1f472e606b
michele-pavlenco <michelepavlenco.mp@gmail.com>
2022-07-22 13:58:44 +0300

    added-login-register

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,13 +6,15 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">{{ __('Login') }}</div>
-
                 <div class="card-body">
+                    <div class="img">
+                        <img src="https://cdn.discordapp.com/attachments/993428523693916180/997097060543254589/deliberboo-delivery-nero.png" alt="">
+                    </div>
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
-
+                        {{-- Email address --}}
                         <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail ') }}</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
@@ -45,29 +47,75 @@
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
                                     <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
+                                        {{ __('Rimani collegato') }}
                                     </label>
                                 </div>
                             </div>
                         </div>
 
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
+                        <div class="login-button d-flex justify-content-center">
+                            <button type="submit" class="  btn btn-outline-light ">
+                                {{ __('Login') }}
+                            </button> 
                         </div>
+
+                        @if (Route::has('password.request'))
+                            <a class="btn btn-link" href="{{ route('password.request') }}">
+                                {{ __('Hai dimenticato la password?') }}
+                            </a>
+                        @endif
+                        
+
+                            </div>
+
+
+
+                        
+
+
                     </form>
                 </div>
             </div>
         </div>
     </div>
 </div>
+<style>
+    :root {
+  --col1: #3f1651;
+  --col2: #653780;
+  --col3: #9c3587;
+  --col4: #e53f71;
+  --col5: #f89f5b;
+}
+*{
+    margin:0 ;
+    padding:0 ;
+    box-sizing: border-box;
+}
+    .card{
+        background-color: var(--col2) !important;
+        color: var(--col5) ;
+    }
+    input{
+        color: var(--col5)
+    }
+    input[type="email"],input[type="password"] {
+        background-color : var(--col5); 
+    }
+    button{
+        /* background-color : var(--col5)!important;  */
+    }
+    .img{
+        text-align: center
+    }
+.img img{
+     height:150px;
+     
+}
+   
+
+
+
+
+</style>
 @endsection
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,9 +1,17 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
+<div class="container my-5">
+    <div class="row justify-content-center debug">
+        <div class="col-md-12">
+
+            <div class="cards-container d-flex">
+
+            <div class="card-text  mx-1">
+                <img src="https://cdn.discordapp.com/attachments/993428523693916180/997097060543254589/deliberboo-delivery-nero.png" alt="logo">
+             <p class="mx-5 d-flex  align-items-center h4">  Hai una fame che fa paura? Deliverboo e l applicazione che fa per te. ordina i tuoi cibi preferiti nei ristoranti che piu ami e noi te li porteremo a casa prima che tu possa dire boo! ...</p> 
+            </div>
+            
             <div class="card">
                 <div class="card-header">{{ __('Register') }}</div>
 
@@ -91,12 +99,12 @@
 
                         <div class="form-group">
                             <h5>Categorie *</h5>
-                            @foreach ($types as $type)
+                            {{-- @foreach ($types as $type)
                                 <div class="form-check-inline">
                                   <input type="checkbox" name="types[]" required data-parsley-mincheck='1' data-parsley-trigger="ready" data-parsley-errors-container="#error" data-parsley-required-message="Inserisci almeno una categoria"class="form-check-input @error('types') is-invalid @enderror" id="{{$type->slug}}" value="{{$type->id}}">
                                   <label class="form-check-label" for="{{$type->slug}}">{{$type->slug}}</label>
                                 </div>
-                            @endforeach
+                            @endforeach --}}
                             <div id="error"></div>
                             @error('types')
                                     <span class="invalid-feedback" role="alert">
@@ -107,15 +115,54 @@
                           <p>I campi segnati con * sono obbligatori</p>
                         <div class="form-group row mb-0">
                             <div class="col-md-6 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn  btn-outline-light">
                                     {{ __('Register') }}
                                 </button>
+
+                                </div>
+
                             </div>
                         </div>
                     </form>
                 </div>
+
+            </div>
+
             </div>
         </div>
     </div>
 </div>
+<style>
+        :root {
+  --col1: #3f1651;
+  --col2: #653780;
+  --col3: #9c3587;
+  --col4: #e53f71;
+  --col5: #f89f5b;
+}
+.debug{
+    border: 1px solid greenyellow;
+}
+.container{
+    width:100%
+}
+.card {
+    background-color: var(--col1);
+    color: var(--col4);
+    width:60%;
+}
+input[type="email"],input[type="password"],input[type="text"] {
+        background-color : var(--col5); 
+    }
+
+    .card-text{
+        width: 40%;
+        color: var(--col5);
+        background-color:var(--col2);
+        text-align: center;
+    }
+    .card-text img{
+        height: 200px
+    }
+</style>
 @endsection
